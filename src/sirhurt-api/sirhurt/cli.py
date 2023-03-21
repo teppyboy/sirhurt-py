9e6555c325b4d5540a3c4ff391afe4964e555a5f
tretrauit <tretrauit@gmail.com>
2023-03-21 11:30:32 +0700

    api/cli: update

--- src/sirhurt-api/sirhurt/cli.py
+++ src/sirhurt-api/sirhurt/cli.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from sirhurt import Exploit, RobloxProcess
 
 exploit = Exploit()
+rbx_proc: RobloxProcess = None
 
 
 def find_process():
@@ -16,6 +17,7 @@ def find_process():
 
 
 def set_process(index):
+    global rbx_proc
     try:
         index = int(index)
     except ValueError:
@@ -34,7 +36,8 @@ def set_process(index):
     return rbx_proc
 
 
-def check_alive(rbx_proc, noisy=False):
+def check_alive(noisy=False):
+    global rbx_proc
     if rbx_proc is None or not rbx_proc.is_process_alive():
         print("Current Roblox process is dead.")
         rbx_proc = None
@@ -44,15 +47,17 @@ def check_alive(rbx_proc, noisy=False):
     return True
 
 
-def close_proc(rbx_proc):
-    if not check_alive(rbx_proc=rbx_proc):
+def close_proc():
+    global rbx_proc
+    if not check_alive():
         return
     rbx_proc.close()
+    rbx_proc = None
     print("Closed current Roblox process.")
 
 
-def inject(rbx_proc):
-    if not check_alive(rbx_proc=rbx_proc):
+def inject():
+    if not check_alive():
         return
     print("Injecting...")
     exploit.inject(roblox=rbx_proc)
@@ -69,8 +74,8 @@ def update_dll():
     print("Update completed.")
 
 
-def execute_file(rbx_proc: RobloxProcess, file):
-    if not check_alive(rbx_proc=rbx_proc):
+def execute_file(file):
+    if not check_alive():
         return
     fp = Path(file)
     if not fp.is_file():
@@ -80,17 +85,62 @@ def execute_file(rbx_proc: RobloxProcess, file):
     print("Execution success.")
 
 
-def loadstring(rbx_proc: RobloxProcess, string):
-    if not check_alive(rbx_proc=rbx_proc):
+def loadstring(string):
+    if not check_alive():
         return
     rbx_proc.execute(string)
     print("Execution success.")
 
+def parse_args(args: list[str]):
+    global rbx_proc
+    try:
+        command = args[0].lower()
+        try:
+            if args[1].lower() == "proc":
+                if command in ["find"]:
+                    find_process()
+                elif command in ["set"]:
+                    rbx_proc = set_process(args[2])
+                elif command in ["check"]:
+                    rbx_proc = (
+                        None if not check_alive(noisy=True) else rbx_proc
+                    )
+                elif command in ["close"]:
+                    close_proc()
+                else:
+                    print("Invalid command:", args[0], args[1])
+                return
+        except Exception:
+            pass
+        if command in ["find-process", "find-proc", "fp"]:
+            find_process()
+        elif command in ["set-process", "set-proc", "sp"]:
+            rbx_proc = set_process(args[1])
+        elif command in ["check-process", "check-proc", "cp"]:
+            rbx_proc = (
+                None if not check_alive(noisy=True) else rbx_proc
+            )
+        elif command in ["close-process", "close-roblox", "close"]:
+            close_proc()
+        elif command in ["update", "upd8", "ud"]:
+            update_dll()
+        elif command in ["inject", "ij"]:
+            inject()
+        elif command in ["execute file", "execfile", "file"]:
+            execute_file(file=args[1])
+        elif command in ["execute", "exec"]:
+            loadstring(string=" ".join(args[1:]))
+        elif command in ["exit"]:
+            print("Exiting from interactive console...")
+            exit(0)
+        else:
+            print("Invalid command:", args[0])
+    except IndexError:
+        print("Error: The command has unfilled args.")
+    except Exception as e:
+        print("Error:", e)
 
 def console():
-    # Variables
-    rbx_proc = None
-    # Code
     print("Welcome to SirHurt API interactive console!")
     print("To get started, type 'help' to show commands list.")
     while True:
@@ -102,35 +152,7 @@ def console():
             print("Exiting from interactive console...")
             break
         args = inp.split(" ")
-        try:
-            command = args[0].lower()
-            if command in ["find-process", "find-proc", "find proc", "fp"]:
-                find_process()
-            elif command in ["set-process", "set-proc", "set proc", "sp"]:
-                rbx_proc = set_process(args[1])
-            elif command in ["check-process", "check-proc", "check proc", "cp"]:
-                rbx_proc = (
-                    None if not check_alive(rbx_proc=rbx_proc, noisy=True) else rbx_proc
-                )
-            elif command in ["close-process", "close-roblox", "close"]:
-                close_proc(rbx_proc=rbx_proc)
-            elif command in ["update", "upd8", "ud"]:
-                update_dll()
-            elif command in ["inject", "ij"]:
-                inject(rbx_proc=rbx_proc)
-            elif command in ["execute file", "execfile", "file"]:
-                execute_file(rbx_proc=rbx_proc, file=args[1])
-            elif command in ["execute", "exec"]:
-                loadstring(rbx_proc=rbx_proc, string=" ".join(args[1:]))
-            elif command in ["exit"]:
-                print("Exiting from interactive console...")
-                break
-            else:
-                print("Invalid command:", args[0])
-        except IndexError:
-            print("Error: The command has unfilled args.")
-        except Exception as e:
-            print("Error:", e)
+        parse_args(args=args)
 
 
 def main():
